28e07b9d546543a4f16df0a6e396f9490eaf56b9
S1ngularty <asherxd10245@gmail.com>
2025-06-27 16:30:10 -0700

    add (backend) : token based auth middleware

---
+++ middlerware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+ exports.isAuth = (req, res, next) => {
+  if (!req.header("Authorization")) {
+    return res.status(401).json({ message: "Unauthorized Access" });
+  }
+
+  const token = req.header('Authorization').split(' ')[1];
+
+  let decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+  console.log(decoded)
+
+  next();
+};
