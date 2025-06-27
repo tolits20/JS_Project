9ef2956323f1abc2858444bab8e10111dcf7e4f0
S1ngularty <asherxd10245@gmail.com>
2025-06-27 14:39:41 -0700

    add (backend): Server initial setup

---
+++ config/database.js
@@ -0,0 +1,2 @@
+const mysql2 = require('mysql2')
+
---
+++ server.js
@@ -0,0 +1,10 @@
+const app =require('./app')
+const port =8080
+
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
+
+app.listen(port,()=>{
+    console.log(`connected to port ${port}`)
+})
