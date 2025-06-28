29101e518cdc1624b50317f4dd5bd080a8765551
S1ngularty <asherxd10245@gmail.com>
2025-06-27 17:17:36 -0700

    add (backend) : role middleware

--- route/admin.js
+++ route/admin.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const route = express.Router()
-
+const authMiddleware=require('../middlerware/auth')
+route.use([authMiddleware])
 route.get('/admin',(req,res)=>{
     res.send("reached")
 })
