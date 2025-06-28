66d8b573f50e33e3f1302665383eb4102293a377
S1ngularty <asherxd10245@gmail.com>
2025-06-28 22:36:35 +0800

    add (frontend): Request class

---
+++ frontend/helper/request.js
@@ -0,0 +1,81 @@
+const request = class {
+  constructor(baseURL, resource, token) {
+    this.baseURL = baseURL;
+    this.token = token;
+    this.resource = resource;
+    this.ipHost = "http://192.168.1.11:8080";
+  }
+
+  __getHeaders() {
+    return {
+      "Cache-Control": "no-cache",
+      Authorization: "Bearer " + this.token,
+    };
+  }
+
+  __formDataConfig(){
+    return {
+        processData:false,
+        ContentType:false,
+    }
+  }
+
+  getAll(success, error) {
+    $.ajax({
+      method: "GET",
+      url: `${this.ipHost}/${this.baseURL}/${this.resource}`,
+      dataType: "json",
+      success: success,
+      error: error,
+    });
+  }
+
+  getById(id, success, error) {
+    $.ajax({
+      method: "GET",
+      url: `${this.ipHost}/${this.baseURL}/${this.resource}/${id}`,
+      dataType: "json",
+      success: success,
+      error: error,
+    });
+  }
+
+  create(data,success,error){
+    $.ajax({
+        method : "POST",
+        url: `${this.ipHost}/${this.baseURL}/${this.resource}`,
+        ...this.__formDataConfig(),
+        headers:this.__getHeaders(),
+        data:data,
+        dataType:'json',
+        success:success,
+        error:error
+    })
+  }
+
+  update(id,data,success,error){
+    $.ajax({
+        method : "POST",
+        url: `${this.ipHost}/${this.baseURL}/${this.resource}/${id}`,
+        ...this.__formDataConfig(),
+        headers:this.__getHeaders(),
+        data:data,
+        dataType:'json',
+        success:success,
+        error:error
+    })
+  }
+
+  delete(id,success,error){
+    $.ajax({
+        method : "DELETE",
+        url: `${this.ipHost}/${this.baseURL}/${this.resource}`,
+        ...this.__formDataConfig(),
+        headers:this.__getHeaders(),
+        dataType:'json',
+        success:success,
+        error:error
+    })
+  }
+
+};
