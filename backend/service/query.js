d4223c23bcf0d288a6f1dec0c4fd6a55e56bd6db
S1ngularty <asherxd10245@gmail.com>
2025-06-30 22:33:40 +0800

    add (backend) : Controller service

---
+++ backend/service/query.js
@@ -0,0 +1,10 @@
+const connection = require("../config/database");
+
+exports.getAll = async (table) => {
+    return result = await connection.query(`SELECT * FROM ${table}`)
+  
+};
+
+exports.getById= async(id,table)=>{
+    return result = await connection.query(`SELECT * FROM ${table} WHERE user_id =${id}`)
+}
