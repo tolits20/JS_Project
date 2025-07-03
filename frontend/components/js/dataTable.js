dbd88cb44184ca4d22d44d3ff7475d2b38884026
S1ngularty <asherxd10245@gmail.com>
2025-07-03 22:08:19 +0800

    refactor (frontend) : Reusability and scalability in core concepts ( DataTable component )

--- frontend/components/js/dataTable.js
+++ frontend/components/js/dataTable.js
@@ -1,24 +1,64 @@
-import {ip,port} from "../../config/network.js"
-console.log(ip,port)
-$(document).ready(function () {
-    console.log('asdsad')
-    $('#u-table').dataTable({
-        ajax:{
-            url:`http://${ip}:${port}/api/v1/admin/user-all`,
-            dataSrc:"data"
-        },
-        buttons:[
-            "pdf",
-            'excel',
-        ],
-        columns:[
-            {data:"name"},
-            {data:'email'},
-            { data:null,
-            render:function(data,type,row){
-                return `<button data-id=${data.user_id} class='btnEdit btn btn-primary'><i class='fa fa-edit'></i></button>
-                <button data-id=${data.user_id} class='btnDestroy btn btn-danger'><i class='fa fa-trash'></i></button>`
-            }}
-        ]
-    })
-});
+import config from "../../config/network.js";
+console.log(config);
+export const network = {
+  ip: config.ip,
+  port: config.port,
+};
+export const dataTable = (url, table) => {
+  console.log(table);
+  let btnUrl = `http://${config.ip}:${config.port}/admin/user/index.html`;
+  if (table === "user") {
+    btnUrl = `http://${config.ip}:${config.port}/admin/user/edit.html`;
+  }
+  let col =
+    table === "user"
+      ? ["name", "email"]
+      : ["item_name", "category", "item_price"];
+  let columns = [];
+  for (let x in col) {
+    columns.push({
+      data: col[x],
+      className: "fw-bold text-capitalize",
+    });
+  }
+  columns.push({
+    data: null,
+    className: "text-end",
+    render: function (data, type, row) {
+      let id = table === "user" ? data.user_id : data.item_id;
+      return `
+            <a data-id="${id}" href="${btnUrl}" class="btnEdit btn btn-sm  me-2">
+              <i class="fa fa-edit"></i> Edit
+            </a>
+            <a data-id="${id}" class="btnDestroy btn btn-sm">
+              <i class="fa fa-trash"></i> Delete
+            </a>`;
+    },
+  });
+  console.log(columns);
+
+  console.log("initializing table...");
+  $(`#${table}`).DataTable({
+    ajax: {
+      url: url,
+      dataSrc: "data",
+    },
+    paging: false,
+    scrollY: 400,
+    dom: "Bfrtip",
+    buttons: [
+      {
+        extend: "pdf",
+        className: "btn btn-dark",
+      },
+      {
+        extend: "excel",
+        className: "btn btn-dark",
+      },
+    ],
+    columns,
+    createdRow: function (row, data, dataIndex) {
+      $(row).addClass("align-middle");
+    },
+  });
+};
