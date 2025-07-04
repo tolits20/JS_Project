418f8f2d87fcffc873a9cce2700d9161dc508f23
S1ngularty <asherxd10245@gmail.com>
2025-07-04 21:23:20 +0800

    add (frontend) : alert pop ups

---
+++ frontend/components/js/alert.js
@@ -0,0 +1,36 @@
+const alert = {
+  positionedDialog: (text) => {
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: `${text}`,
+      showConfirmButton: false,
+      timer: 1500,
+    });
+  },
+  deleteConfirmation: (confirmText,text) => {
+    return new Promise ((resolve,rejects)=>{
+        Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: `${confirmText}`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: `${text}`,
+          icon: "success",
+        });
+        resolve(true)
+      }
+       rejects(false)
+    });
+    })
+  },
+};
+
+export default alert;
