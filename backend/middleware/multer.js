febe7b1b8837a27f0cd977067bda481c9ff5dc75
S1ngularty <asherxd10245@gmail.com>
2025-07-06 14:01:52 +0800

    refactor (backend) : multer middleware dynamic paths

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,23 +1,36 @@
 const multer = require("multer");
 const path = require("path");
 
+const filePaths = {
+  user: "storage/images",
+  item: {
+    main: "storage/items/main",
+    gallery: "storage/items/gallery",
+  },
+};
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    console.log(req.body, file);
-    cb(null, "storage/images");
+    // console.log("start", req.params.flag, "<=>", file, "end");
+    let dest;
+    if (req.params.flag == undefined || req.params.flag == null) {
+      dest = filePaths.user;
+    } else {
+      dest = req.params.flag ==="item" ?  filePaths.item.main : filePaths.item.gallery;
+    }
+    cb(null, dest);
   },
   filename: (req, file, cb) => {
     let dateSuffix = Date.now();
     const ext = path.extname(file.originalname).toLocaleLowerCase();
     const filename = path.parse(file.originalname).name;
-    console.log(path.parse(file.originalname));
+
     cb(null, filename + "-" + dateSuffix + ext);
   },
 });
 module.exports = multer({
   storage: storage,
   fileFilter: (req, file, cb) => {
-    console.log("here", file);
     const ext = path.extname(file.originalname).toLocaleLowerCase();
     if (ext !== "jpg" || ext !== "jpeg" || ext !== "png") cb(null, false);
     cb(null, true);
