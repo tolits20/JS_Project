209607cf6873d873fc9ecad713f63047bae96096
S1ngularty <asherxd10245@gmail.com>
2025-07-18 10:33:59 +0800

    feat (frontend) : redirection utility errorStatus function, it helps to redirects to the error status pages if ever the request fails

--- frontend/utils/redirection.js
+++ frontend/utils/redirection.js
@@ -2,10 +2,16 @@ import network from "../config/network.js";
 
 const redirects = {
   unauthorize: () => {
+    location.href = `http://${network.client.host}/frontend/status_pages/401.html`;
+  },
+  forbidden: () => {
     location.href = `http://${network.client.host}/frontend/status_pages/403.html`;
   },
   notFound: () => {
     location.href = `http://${network.client.host}/frontend/status_pages/404.html`;
+  },
+  InternalServerErorr: () => {
+    location.href = `http://${network.client.host}/frontend/status_pages/500.html`;
   },
 };
 
@@ -19,4 +25,20 @@ const roleCheck = (role) => {
   }
 };
 
-export default roleCheck;
+const errorStatus = (status) => {
+  switch (status) {
+    case 403:
+      redirects.forbidden();
+      break;
+    case 404:
+      redirects.notFound();
+      break;
+    case 500:
+      redirects.InternalServerErorr();
+      break;
+    case 401:
+      redirects.unauthorize();
+  }
+};
+
+export { errorStatus, roleCheck };
