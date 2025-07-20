16fbf594111b7c1f988a78af4814e0cfd8dbda57
S1ngularty <asherxd10245@gmail.com>
2025-07-20 12:22:00 +0800

    refactor (fronend) : encapsulate and modularize the sidebar links to scale and optimize

---
+++ frontend/js/sidebar-links.js
@@ -0,0 +1,41 @@
+import network from "../config/network.js";
+import logout from "./logout.js";
+
+const sidebarLinks = () => {
+  document
+    .getElementById("sidebar-dashboard")
+    .setAttribute(
+      "href",
+      `http://${network.client.host}/frontend/admin/dashboard.html`
+    );
+  document
+    .getElementById("sidebar-user")
+    .setAttribute(
+      "href",
+      `http://${network.client.host}/frontend/admin/user/index.html`
+    );
+  document
+    .getElementById("sidebar-item")
+    .setAttribute(
+      "href",
+      `http://${network.client.host}/frontend/admin/item/index.html`
+    );
+  document
+    .getElementById("sidebar-category")
+    .setAttribute(
+      "href",
+      `http://${network.client.host}/frontend/admin/category/index.html`
+    );
+  document
+    .getElementById("sidebar-order")
+    .setAttribute(
+      "href",
+      `http://${network.client.host}/frontend/admin/orders/index.html`
+    );
+  document.getElementById("sidebar-logout").addEventListener("click", (e) => {
+    e.preventDefault();
+    logout();
+  });
+};
+
+export default sidebarLinks;
